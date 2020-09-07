4628ebeb3270af1ce03af2804de8c995fdc13bf0
jutelles <julianatelles.contato@gmail.com>
2020-09-06 22:47:52 -0300

    fix dotenv

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-app-back-end.herokuapp.com/',
+  baseURL: 'https://grades-app-back-end.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
